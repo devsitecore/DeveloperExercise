93914e32f8af2a58b477ee06851224100d7cedcd
Azhar Javaid Abbasi <azhar.javaid@gmail.com>
2018-05-06 04:38:07 +0800

    Removed empty file

--- Src/AirportsFeedReader.Web/Attributes/FromFeedHeaderAttribute.cs
+++ Src/AirportsFeedReader.Web/Attributes/FromFeedHeaderAttribute.cs
@@ -1,7 +1,49 @@
-﻿using Foundation.Contracts;
-using System.Web.Mvc;
+﻿// <copyright file="FromFeedHeaderAttribute.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace AirportsFeedReader.Web
 {
+    using System.Web.Mvc;
+    using Common.Extensions;
+    using Common.Unity;
+    using Foundation.Contracts;
+    using Foundation.Model;
 
+    public class FromFeedHeaderAttribute : ActionFilterAttribute
+    {
+        private readonly string headerName = "from-feed";
+
+        public FromFeedHeaderAttribute()
+        {
+            var cacheHandler = DependencyInjection.Instance().Container.Resolve<ICacheHandler>();
+            this.Init(cacheHandler);
+        }
+
+        public FromFeedHeaderAttribute(ICacheHandler cacheHandler)
+        {
+            this.Init(cacheHandler);
+        }
+
+        private ICacheHandler CacheHandler { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var cacheData = this.CacheHandler.GetData();
+
+            var feedSource = FeedSource.Cache;
+
+            if (string.IsNullOrEmpty(cacheData))
+            {
+                feedSource = FeedSource.Feed;
+            }
+
+            context.HttpContext.Response.Headers.Add(this.headerName, feedSource.ToString());
+        }
+
+        protected void Init(ICacheHandler cacheHandler)
+        {
+            this.CacheHandler = cacheHandler;
+        }
+    }
 }
